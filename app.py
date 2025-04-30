f6580e5ba9e9f7e21958b6303310a4bc546238cc
Chitransh Chocksey <chitransh33333@gmail.com>
2025-04-30 23:56:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,4 +45,7 @@ def after_request(response):
     return response
 
 if __name__ == '__main__':
-    app.run()
+    import os
+port = int(os.environ.get("PORT", 10000))
+app.run(host='0.0.0.0', port=port)
+
